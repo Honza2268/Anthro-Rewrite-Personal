f666eeb5e5fc647a0b3db972073d9ed839207564
Honza2268 <mrak0020@gmail.com>
2021-11-14 22:54:23 +0100

    initial commit

---
+++ Textures/Anthro/Heads/remove_redundant.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import os
+
+os.chdir('G:\SteamLibrary\steamapps\common\RimWorld\Mods\Anthro-Rewrite\Textures\Anthro\Heads')
+
+files = [f for f in os.listdir() if '.png' in f]
+
+groups = [f.split('_')[1] for f in files]
+groups = list(set(groups))
+
+bad = [files[f]
+       for f in range(len(files)) if 'm.png' in files[f] or 'Female' in files[f]]
+
+for f in bad:
+    os.remove(f)
+
+files = [f for f in os.listdir() if '.png' in f]
+
+for f in files:
+    _, name = f.split('_', 1)
+    os.rename(f, name)
